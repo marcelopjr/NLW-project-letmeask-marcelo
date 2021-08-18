6b57a3252c556f0d15ec7d3bb26b67e9321c3f8f
mpj144 <marcelopjrdev@gmail.com>
2021-08-17 21:13:21 -0300

    pausa

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { type } from "os"
+
+import { ButtonHTMLAttributes } from 'react'
+
+import '../styles/button.scss'
+
+type ButtonPros = ButtonHTMLAttributes<HTMLButtonElement>
 
 
-export function Button() {
 
-    const [number, setNumber] = useState(0)
-
-    function increment(){
-        setNumber(number + 1);
-    }
-
+export function Button(props: ButtonPros) {
     return (
-        <button onClick={increment} >{number}</button>
+        <button className="button" {...props} />
+
     )
 }
